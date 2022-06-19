638c710a9199a34aa8fec8dc34db239d8e4b7559
Robert L <robertmorales181@gmail.com>
2022-06-19 15:57:39 -0600

    list home added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,46 @@
+import 'package:facuturas_registro/models/models.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  final options = const [
-    "Ver facturas",
-    "Ingresar Facturas",
-    "Agrupado Proveedor",
-    "Resumen",
-    "Proveedor",
-    "Categoría",
-    "Configuración"
+  //List with menuOptions model
+  final menuOptions = const [
+    MenuOption(
+      title: "Ver facturas",
+      icon: Icons.list,
+      route: "facturas",
+    ),
+    MenuOption(
+      title: "Ingresar Facturas",
+      icon: Icons.add,
+      route: "ingresar",
+    ),
+    MenuOption(
+      title: "Agrupado Proveedor",
+      icon: Icons.group_work,
+      route: "agrupado",
+    ),
+    MenuOption(
+      title: "Resumen",
+      icon: Icons.assessment,
+      route: "resumen",
+    ),
+    MenuOption(
+      title: "Proveedor",
+      icon: Icons.person,
+      route: "proveedor",
+    ),
+    MenuOption(
+      title: "Categoría",
+      icon: Icons.category,
+      route: "categoria",
+    ),
+    MenuOption(
+      title: "IVA",
+      icon: Icons.attach_money,
+      route: "iva",
+    )
   ];
 
   @override
@@ -18,6 +48,22 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text("Opciones")),
+      ),
+      body: ListView.separated(
+        itemCount: menuOptions.length,
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(menuOptions[index].title),
+            leading: Icon(menuOptions[index].icon),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.pushNamed(context, menuOptions[index].route);
+            },
+          );
+        },
       ),
     );
   }
